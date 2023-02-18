a4c2c558bfd8f8c6eb4dd87f35a36485be784c27
Vaibhav Mor <thevaimor@icloud.com>
2023-02-18 14:35:42 +1300

    Add display existing notes (from local array) functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import { useState } from 'react';
 import './App.css';
 
+interface Note {
+  id: number;
+  title: string;
+  content: string;
+}
+
 function App() {
+  const [notes, setNotes] = useState<Note[]>([]);
 
   return (
     <div className="App">
+      <div className="NotesSection">
+        <h3>Your Notes:</h3>
+        <ul>
+          {notes.map((note) => (
+            <li key={note.id}>
+              <h4>{note.title}</h4>
+              <p>{note.content}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
