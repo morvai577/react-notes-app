50550ac5e4df16323d03f0569c020f6389fc84c6
Vaibhav Mor <thevaimor@icloud.com>
2023-02-18 14:36:38 +1300

    Add create note (to local array) functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+
 import { useState } from 'react';
 import './App.css';
 
@@ -8,6 +10,8 @@ interface Note {
 }
 
 function App() {
+  const [noteTitle, setNoteTitle] = useState('');
+  const [noteContent, setNoteContent] = useState('');
   const [notes, setNotes] = useState<Note[]>([]);
 
   return (
@@ -22,6 +26,43 @@ function App() {
             </li>
           ))}
         </ul>
+      </div>
+      <div className="CreateNoteSection">
+        <h3>Create new note</h3>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            const newNote = {
+              id: Date.now(),
+              title: noteTitle,
+              content: noteContent,
+            };
+            setNotes([...notes, newNote]);
+            setNoteTitle('');
+            setNoteContent('');
+          }}
+        >
+          <label htmlFor="note-title">Title:</label>
+          <input
+            type="text"
+            id="note-title"
+            value={noteTitle}
+            required
+            onChange={(event) => {
+              setNoteTitle(event.target.value);
+            }}
+          />
+          <label htmlFor="note-content">Content:</label>
+          <input
+            type="textarea"
+            id="note-title"
+            value={noteContent}
+            onChange={(event) => {
+              setNoteContent(event.target.value);
+            }}
+          />
+          <button type="submit">Create</button>
+        </form>
       </div>
     </div>
   );
